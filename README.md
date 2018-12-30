dd9e53a3e4b96b152ce59792cd3dd2c53fa3793a
prosenkranz <prosenkranz@mail.de>
2018-12-30 04:15:16 +0100

    Implement basic functionality for showing twitch messages

---
+++ README.md
@@ -0,0 +1,5 @@
+## Usage
+
+* Copy `config.default.js` to `config.local.js` and change credentials, channel and other settings as needed.
+
+* Open in browser
---
+++ config.default.js
@@ -0,0 +1,5 @@
+var CLIENT_ID = "ccxk8gzqpe0qd8t5lmwf45t1kplfi1";
+var CHANNEL = "<replace>";
+var USERNAME = "<replace>";
+var OAUTH_TOKEN = "<replace>";
+var MAX_MESSAGES = 100;
